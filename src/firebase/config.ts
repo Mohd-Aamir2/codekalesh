50e66c700bb0930cb981074e1049e88c1f08e937
alfa <alfa298602@gmail.com>
2025-11-14 02:29:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-362297702-54254",
+  "appId": "1:3990698578:web:65c7f8559fb6dd81fbfd8b",
+  "apiKey": "AIzaSyB40Cg9mssAKjAzrqZPCVCg5_Q9krlOduM",
+  "authDomain": "studio-362297702-54254.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "3990698578"
+};
